6faf3729b7e2a4b750162265eaef1361c8b4e8ba
UserUNP <39243708+UserUNP@users.noreply.github.com>
2025-07-25 18:21:57 +0100

    rah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,13 @@
-// @ts-checkimport { defineConfig } from 'astro/config';import react from '@astrojs/react';import tailwindcss from '@tailwindcss/vite';// https://astro.build/configexport default defineConfig({  integrations: [react()],  vite: {    plugins: [tailwindcss()],    ssr: {      noExternal: ['webcoreui']    }  }});
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+// https://astro.build/config
+export default defineConfig({
+	site: 'soltrubeg.github.io',
+	base: 'tqutils',
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+	}
+});
